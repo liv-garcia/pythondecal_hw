64e107d8130f65a5d053d15cf3a809c3db02c118
liv-garcia <oliviarg098@gmail.com>
2024-09-26 15:36:20 -0700

    testing github

---
+++ cond_loops_disc.py
@@ -0,0 +1,44 @@
+### This is the syntax of a for-loop ###
+
+#for variable in range(start, stop, step):
+    # Code block to be executed
+
+### This is the syntax of a while-loop ###
+
+#while condition == true:
+    # Code block to be executed
+
+
+# Note: It is normal to forget the syntax! That is why google is your bff and you just need to google "for loop syntax" or "while loop syntax"
+# and there are tons of resources. I usually just go to images to quickly check the syntax.
+
+
+'''### Problem 1: Your turn!
+
+Print i 10 times using both a for and while loop.'''
+# for loop bellow
+i = any 
+while  i = any
+print(i**10)
+i-=i
+
+#while loop below
+for i in range(i,i+1)
+print(i)
+
+
+
+'''###Problem 2: Range()
+Range is a cool function but there are a lot of parts to it so let's disect it!
+
+What happens if the range is from (5, 5)?  How about from (10, 6)?  
+Try different step sizes: (10, 6, -1), (1, 10, 2). Write down all your observations and answer the following questions.'''
+
+#YOUR CODE HERE
+range(5, 5)
+len(range(5, 5))
+
+'''### Problem 3: Listness
+How do you loop through elements in a list using a for loop? '''
+
+#YOUR CODE HERE
